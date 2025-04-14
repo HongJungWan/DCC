da2ac0a295f63eb9c0a30e995cf3a81dad9b8af3
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2025-04-14 23:29:50 +0900

    feat: 사다리 타기 문제 PS

---
+++ src/step1/Ps01.java
@@ -0,0 +1,33 @@
+package step1;
+
+import java.util.Arrays;
+
+/**
+ * 사다리 타기
+ */
+public class Ps01 {
+    public char[] PS01(int n, int[][] ladder){
+        char[] answer = new char[n];
+
+        for(int i = 0; i < n; i++){
+            answer[i] = (char)(i + 65);
+        }
+
+        for(int[] line : ladder){
+            for(int x : line){
+                char tmp = answer[x];
+                answer[x] = answer[x-1];
+                answer[x-1] = tmp;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Ps01 T = new Ps01();
+        System.out.println(Arrays.toString(T.PS01(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
+        System.out.println(Arrays.toString(T.PS01(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
+        System.out.println(Arrays.toString(T.PS01(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
+        System.out.println(Arrays.toString(T.PS01(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
+    }
+}
