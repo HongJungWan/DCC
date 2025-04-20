7425e9bd13ee18fa58fb5d88ffcb3163557df06a
HONG-JUNG-WAN <hong43ok@gmail.com>
2025-04-20 11:01:48 +0900

    feat: [재도전] 사다리 타기 PS 구현

--- src/re_step1/Ps01.java
+++ src/re_step1/Ps01.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 public class Ps01 {
     public static void main(String[] args) {
         step1.Ps01 T = new step1.Ps01();
+
         System.out.println(Arrays.toString(T.ps01(5, new int[][]{{1, 3}, {2, 4}, {1, 4}})));
         System.out.println(Arrays.toString(T.ps01(7, new int[][]{{1, 3, 5}, {1, 3, 6}, {2, 4}})));
         System.out.println(Arrays.toString(T.ps01(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
@@ -17,6 +18,17 @@ public class Ps01 {
     public char[] ps01(int n, int[][] ladder) {
         char[] answer = new char[n];
 
+        for (int i = 0; i < n; i++) {
+            answer[i] = (char) (i + 65);
+        }
+
+        for (int[] items : ladder) {
+            for (int item : items) {
+                char tmp = answer[item];
+                answer[item] = answer[item - 1];
+                answer[item - 1] = tmp;
+            }
+        }
         return answer;
     }
 }
